4f4526d970a405a3f09b90e7760a944f88416e31
William Blackerby <wmblackerby@gmail.com>
2025-03-05 23:04:16 -0500

    change neo tree keybind and fix colorscheme

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -11,15 +11,7 @@ return {
   },
   cmd = 'Neotree',
   keys = {
-    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+    { '<leader>e', ':Neotree filesystem toggle reveal float<cr>', desc = 'NeoTree toggle', silent = true },
   },
-  opts = {
-    filesystem = {
-      window = {
-        mappings = {
-          ['\\'] = 'close_window',
-        },
-      },
-    },
-  },
+  opts = {},
 }
